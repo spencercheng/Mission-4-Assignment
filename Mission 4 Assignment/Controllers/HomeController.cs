41d9a02120d615ccbc5f41021c877b19ad7e4206
Spencer Cheng <89827794+spencercheng@users.noreply.github.com>
2023-01-24 11:07:54 -0700

    Mission 5 MVC Assignment

---
+++ Mission 4 Assignment/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_4_Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GradingCalculator()
+        {
+            return View();
+        }
+    }
+}
