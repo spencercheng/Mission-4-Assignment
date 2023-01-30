20ac0fb68550d97d542a39f056000428521e67e2
Spencer Cheng <89827794+spencercheng@users.noreply.github.com>
2023-01-30 13:12:42 -0700

    yo

--- Mission 4 Assignment/Models/GradingCalculatorModel.cs
+++ Mission 4 Assignment/Models/GradingCalculatorModel.cs
@@ -8,13 +8,17 @@ namespace Mission_4_Assignment.Models
 {
     public class GradingCalculatorModel
     {
-        [Required]
-        
+        [Range(0,100)]
         public int Assignment { get; set; }
+        [Range(0, 100)]
         public int GroupProject { get; set; }
+        [Range(0, 100)]
         public int Quizzes { get; set; }
+        [Range(0, 100)]
         public int Midterm { get; set; }
+        [Range(0, 100)]
         public int Final { get; set; }
+        [Range(0, 100)]
         public int Intex { get; set; }
 
     }
--- Mission 4 Assignment/wwwroot/js/Calculator.js
+++ Mission 4 Assignment/wwwroot/js/Calculator.js
@@ -1,64 +1,53 @@
 ﻿
-$("#send").submit(function () {
-        // Getting the form data and mutiplying by the percentage
-        var Assignments = ($("#assignment").val() * .50);
-        var GroupProject = ($("#GroupProject").val() * .10);
-        var Quizzes = ($("#Quizzes").val() * .10);
-        var Midterm = ($("#Midterm").val() * .10);
-        var FinalExam = ($("#Final").val() * .10);
-        var Intex = ($("#Intex").val() * .10);
+$("#send").click(function () {
+    // Getting the form data and mutiplying by the percentage
+    var Assignments = ($("#assignment").val() * .50);
+    var GroupProject = ($("#GroupProject").val() * .10);
+    var Quizzes = ($("#Quizzes").val() * .10);
+    var Midterm = ($("#Midterm").val() * .10);
+    var FinalExam = ($("#Final").val() * .10);
+    var Intex = ($("#Intex").val() * .10);
 
-        // adding it all together
-        var Final = Assignments + GroupProject + Quizzes + Midterm + FinalExam + Intex;
-        console.log(Final)
+    // adding it all together
+    var Final = Assignments + GroupProject + Quizzes + Midterm + FinalExam + Intex;
 
-        // output base on condition.
-        if (Final >= 94.0) {
-            $("#grade").text("Final Grade: A");
-            $("#percent").text("Percent: " + Final + "%");
-        }
-        else if (Final >= 90.0 && Final < 94.0) {
-            $("#grade").text("Final Grade: A-");
-            $("#percent").text("Percent: " + Final + "%");
-        }
-        else if (Final >= 87.0 && Final < 90.0) {
-            $("#grade").text("Final Grade: B+");
-            $("#percent").text("Percent: " + Final + "%");
-        }
-        else if (Final >= 84.0 && Final < 87.0) {
-            $("#grade").text("Final Grade: B");
-            $("#percent").text("Percent: " + Final + "%");
-        }
-        else if (Final >= 80.0 && Final < 84.0) {
-            $("#grade").text("Final Grade:  B-");
-            $("#percent").text("Percent: " + Final + "%");
-        }
-        else if (Final >= 77.0 && Final < 80.0) {
-            $("#grade").text("Final Grade: C+");
-            $("#percent").text("Percent: " + Final + "%");
-        }
-        else if (Final >= 74.0 && Final < 77.0) {
-            $("#grade").text("Final Grade: C");
-            $("#percent").text("Percent: " + Final + "%");
-        }
-        else if (Final >= 70.0 && Final < 74.0) {
-            $("#grade").text("Final Grade: C-");
-            $("#percent").text("Percent: " + Final + "%");
-        }
-        else if (Final >= 67.0 && Final < 70.0) {
-            $("#grade").text("Final Grade: D+");
-            $("#percent").text("Percent: " + Final + "%");
-        }
-        else if (Final >= 64.0 && Final < 67.0) {
-            $("#grade").text("Final Grade: D");
-            $("#percent").text("Percent: " + Final + "%");
-        }
-        else if (Final >= 60.0 && Final < 64.0) {
-            $("#grade").text("Final Grade: D-");
-            $("#percent").text("Percent: " + Final + "%");
-        }
-        else {
-            $("#grade").html("Final Grade: E");
-            $("#percent").html("Percent: " + Final + "%");
-        }
-})
+    if (Final >= 94) {
+        letter = "A"
+    } else if (90 <= Final && Final < 94) {
+        letter = "A-"
+    }
+    else if (87 <= Final && Final < 90) {
+        letter = "B+"
+    }
+    else if (84 <= Final && Final < 87) {
+        letter = "B"
+    }
+    else if (80 <= Final && Final < 84) {
+        letter = "B-"
+    }
+    else if (77 <= Final && Final < 80) {
+        letter = "C+"
+    }
+    else if (74 <= Final && Final < 77) {
+        letter = "C"
+    }
+    else if (70 <= Final && Final < 74) {
+        letter = "C-"
+    }
+    else if (67 <= Final && Final < 70) {
+        letter = "D+"
+    }
+    else if (64 <= Final && Final < 67) {
+        letter = "D"
+    }
+    else if (60 <= Final && Final < 64) {
+        letter = "D-"
+    }
+    else if (Final < 60) {
+        letter = "E"
+    }
+
+    //display the final Finals in an alert message
+
+    alert("Final Grade: " + Final + " (" + letter + ")");
+});
